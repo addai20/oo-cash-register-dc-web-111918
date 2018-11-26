052daa463c39e35d961188b84baf35c5c6c8d10f
Benjamin Addai <addai.5@buckeyemail.osu.edu>
2018-11-26 18:46:21 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
-
+class CashRegister
+  attr_writer 
+  attr_accessor :total, :price, 
+  
+  
+  
+  def initialize(total = 0)
+    @total = total
+  end
+  
+  def total
+    
+    
+  end
+  
+  def add_item(title, price)
+  
+  end
+end
